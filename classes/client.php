158ae9a1ebb319708914ded37d73bb4b97dd470b
Eashune Abenojar <EashuneAbenojar@hotmail.com>
2021-06-14 08:11:55 -0400

    added Objects

--- classes/client.php
+++ classes/client.php
@@ -1,1 +1,148 @@
 <?php
+
+class Client
+{
+    private $_fName;
+    private $_lName;
+    private $_pNum;
+    private $_make;
+    private $_model;
+    private $_year;
+    private $_miles;
+
+    /**
+     * Clients constructor.
+     * @param $_fName
+     * @param $_lName
+     * @param $_pNum
+     * @param $_make
+     * @param $_model
+     * @param $_year
+     * @param $_miles
+     */
+    public function __construct($_fName = "", $_lName = "", $_pNum = "",
+                                $_make = "", $_model = "", $_year = 0, $_miles = 0)
+    {
+        $this->_fName = $_fName;
+        $this->_lName = $_lName;
+        $this->_pNum = $_pNum;
+        $this->_make = $_make;
+        $this->_model = $_model;
+        $this->_year = $_year;
+        $this->_miles = $_miles;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getFName(): string
+    {
+        return $this->_fName;
+    }
+
+    /**
+     * @param mixed|string $fName
+     */
+    public function setFName($fName): void
+    {
+        $this->_fName = $fName;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getLName(): string
+    {
+        return $this->_lName;
+    }
+
+    /**
+     * @param mixed|string $lName
+     */
+    public function setLName($lName): void
+    {
+        $this->_lName = $lName;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getPNum(): string
+    {
+        return $this->_pNum;
+    }
+
+    /**
+     * @param mixed|string $pNum
+     */
+    public function setPNum($pNum): void
+    {
+        $this->_pNum = $pNum;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getMake(): string
+    {
+        return $this->_make;
+    }
+
+    /**
+     * @param mixed|string $make
+     */
+    public function setMake($make): void
+    {
+        $this->_make = $make;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getModel(): string
+    {
+        return $this->_model;
+    }
+
+    /**
+     * @param mixed|string $model
+     */
+    public function setModel($model): void
+    {
+        $this->_model = $model;
+    }
+
+    /**
+     * @return int|mixed
+     */
+    public function getYear(): int
+    {
+        return $this->_year;
+    }
+
+    /**
+     * @param int|mixed $year
+     */
+    public function setYear($year): void
+    {
+        $this->_year = $year;
+    }
+
+    /**
+     * @return int|mixed
+     */
+    public function getMiles(): int
+    {
+        return $this->_miles;
+    }
+
+    /**
+     * @param int|mixed $miles
+     */
+    public function setMiles($miles): void
+    {
+        $this->_miles = $miles;
+    }
+
+
+}
--- classes/warrantyClient.php
+++ classes/warrantyClient.php
@@ -1,1 +1,75 @@
 <?php
+
+class WarrantyClient extends Client
+{
+    private $_wash;
+    private $_vacuum;
+    private $_buff;
+
+    /**
+     * WarrantyClient constructor.
+     * @param $_wash
+     * @param $_vacuum
+     * @param $_buff
+     */
+    public function __construct($_fName = "", $_lName = "", $_pNum = "",
+                                $_make = "", $_model = "", $_year = 0,
+                                $_miles = 0, $_wash = "", $_vacuum = "", $_buff = "")
+    {
+        parent::__construct($_fName, $_lName, $_pNum,
+            $_make, $_model, $_year, $_miles);
+        $this->_wash = $_wash;
+        $this->_vacuum = $_vacuum;
+        $this->_buff = $_buff;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getWash(): string
+    {
+        return $this->_wash;
+    }
+
+    /**
+     * @param mixed|string $wash
+     */
+    public function setWash($wash): void
+    {
+        $this->_wash = $wash;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getVacuum(): string
+    {
+        return $this->_vacuum;
+    }
+
+    /**
+     * @param mixed|string $vacuum
+     */
+    public function setVacuum($vacuum): void
+    {
+        $this->_vacuum = $vacuum;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getBuff(): string
+    {
+        return $this->_buff;
+    }
+
+    /**
+     * @param mixed|string $buff
+     */
+    public function setBuff($buff): void
+    {
+        $this->_buff = $buff;
+    }
+
+
+}
