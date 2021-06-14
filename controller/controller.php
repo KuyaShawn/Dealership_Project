af0d394cf4b222ff7c83cc10069c71b346291da0
David Pavlenko <Dpavlenko@mail.greenriver.edu>
2021-06-13 21:23:19 -0400

    added a controller and also updated the index page for the controller

--- controller/controller.php
+++ controller/controller.php
@@ -1,1 +1,53 @@
 <?php
+
+class dealerController
+{
+    private $_f3;
+
+    public function __construct($f3)
+    {
+        $this->_f3 = $f3;
+    }
+
+    public function home()
+    {
+        // Display the home page
+        $view = new Template();
+        echo $view -> render('views/home.html');
+    }
+
+    public function inventory()
+    {
+        // Inventory page
+        $view = new Template();
+        echo $view->render('views/inventory.html');
+    }
+
+    public function services()
+    {
+        //Display the home page
+        $view = new Template();
+        echo $view->render('views/services.html');
+    }
+
+    public function parts()
+    {
+        //Display the home page
+        $view = new Template();
+        echo $view->render('views/parts.html');
+    }
+
+    public function admin()
+    {
+        //Display the home page
+        $view = new Template();
+        echo $view->render('views/admin.html');
+    }
+
+    public function login()
+    {
+        //Display the home page
+        $view = new Template();
+        echo $view->render('views/admin_login.html');
+    }
+}
--- index.php
+++ index.php
@@ -7,54 +7,42 @@ error_reporting(E_ALL);
 //Require autoload file
 require_once ('vendor/autoload.php');
 
+//start the session
+session_start();
+
 //Instantiate Fat-Free
 $f3 = Base::instance();
 
+$controller = new dealerController($f3);
+
 //Home page
 $f3->route('GET /', function(){
-    // Display the home page
-    $view = new Template();
-    echo $view -> render('views/home.html');
+    $GLOBALS['controller']->home();
 });
 
 // Inventory page
 $f3->route('GET /inventory', function(){
-
-
-    $view = new Template();
-    echo $view->render('views/inventory.html');
+    $GLOBALS['controller']->inventory();
 });
 
 // Service page
 $f3->route('GET /services', function(){
-
-    //Display the home page
-    $view = new Template();
-    echo $view->render('views/services.html');
+    $GLOBALS['controller']->services();
 });
 
 // parts page
 $f3->route('GET /parts', function(){
-
-    //Display the home page
-    $view = new Template();
-    echo $view->render('views/parts.html');
+    $GLOBALS['controller']->parts();
 });
 
 // Admin page
 $f3->route('GET /admin', function(){
-
-    //Display the home page
-    $view = new Template();
-    echo $view->render('views/admin.html');
+    $GLOBALS['controller']->admin();
 });
 
 // Admin login page
 $f3->route('GET /admin_login', function(){
-
-    //Display the home page
-    $view = new Template();
-    echo $view->render('views/admin_login.html');
+    $GLOBALS['controller']->login();
 });
 
 //Run Fat-Free
